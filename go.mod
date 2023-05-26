f58adbb6024ee5a861da60d939295db627e47b64
Aman Arora <aman.arora@siemens-healthineers.com>
2023-05-26 11:38:32 +0530

    third

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/arora8055/puppy
 
 go 1.20
+
+require github.com/arora8055/Dog v0.0.0-20230526060013-8164f5a84f76 // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
+import (
+	dog "github.com/arora8055/Dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBarks() string {
+	return dog.WhenGrownUp(Barks())
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
 }
